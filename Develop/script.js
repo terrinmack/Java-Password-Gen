63cd791f9b2ae613e9cf344de76ad8e46c4dccde
Terri Mack <terrimack15@gmail.com>
2023-01-16 18:15:57 -0800

    Edited java file with starting functions.

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,31 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// array variables
+var lowerCase ="abdefghifjlkmnopqrstuvwxyz";
+var upperCase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numeric = "0123456789";
+var special = "!@#$%^&*()-_=+[]{};:'<>/?\|`~.";
+
+// main functions
+function generatePassword() {
+  var password = "";
+  var newPassword = "";
+
+// password length prompts with error prompt
+var passwordLength = window.prompt("How many characters do you want for your password?(Choose an integer between 8-128");
+  while ((passwordLength < 8) || (passwordLegth > 128)) {
+    window.alert("Please try again. Select a value between 8-128");
+    passwordLength = window.prompt("How many characters do you want for your password?(Choose an integer between 8-128");
+  }
+
+// password criteria functions
+function passwordCriteria() {
+  includeUpperCase = window.confirm ("Include uppercase letters?\nType \"YES\" or \"NO\"");
+  if (includeUpperCase) {
+    
+  }
+}
+}
